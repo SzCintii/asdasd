7ea1dfedd281b84e5fdec0553f0cd9eabc815464
rcfr84 <valcsi20010923@gmail.com>
2022-10-12 15:11:50 +0200

    A kódban szereplő felesleges megjegyzések törlése.

--- backend/menu.php
+++ backend/menu.php
@@ -7,9 +7,7 @@
 
             break;
         case 'user':
-            //echo "<a href='index.php?page=35'>Profilom</a><br>";
             echo "<a href='index.php?page=10'>Profilom szerkesztése</a><br>";
-            //echo "<a href='index.php?page=25'>Kölcsönzéseim</a><br>";
             echo "<a href='index.php?page=450'>Filmek</a><br>";
             break;
     }
