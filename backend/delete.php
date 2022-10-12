411134b49401b0e1e31456bac24a3f8c262c15c8
rcfr84 <valcsi20010923@gmail.com>
2022-10-12 15:07:52 +0200

    A delete.php felesleges részek törlése.

--- backend/delete.php
+++ backend/delete.php
@@ -3,8 +3,6 @@
     $con = connect('user','root','');
     $filmId = $_POST['film_id'];
     $userId = $_SESSION['id'];
-    //$query="DELETE FROM motorok(motorId, userId) VALUES('$motorId','$userId') 
-    //WHERE motorid=".$_POST['motorId'];
     $query="DELETE FROM film WHERE film_id=".$_POST['film_id'];
     $result = mysqli_query($con, $query) or die ("Nem sikerült ".$query);
     header('Location: index.php');
