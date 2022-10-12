b839677b7592ff28616f8058cf66c9735cf5d514
rcfr84 <valcsi20010923@gmail.com>
2022-10-12 15:10:46 +0200

    Tisztább kód elkészítése.

--- backend/menu.php
+++ backend/menu.php
@@ -2,9 +2,7 @@
     switch($_SESSION['jog']) {
         case 'mod':
             echo "<a href='index.php?page=100'>Új film hozzáadása</a><br>";
-            //echo "<a href='index.php?page=35'>Profilom</a><br>";
             echo "<a href='index.php?page=10'>Profilom szerkesztése</a><br>";
-            //echo "<a href='index.php?page=25'>Kölcsönzéseim</a><br>";
             echo "<a href='index.php?page=450'>Filmek</a><br>";
 
             break;
